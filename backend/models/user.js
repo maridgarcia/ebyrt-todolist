ea5e5cf4952c7d35418930b1b28e4e2673151a83
Mariana Garcia <maridgarcia045@gmail.com>
2022-05-19 10:41:39 -0300

    feat: 🎸 function to create user on model layer

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,23 @@
-// const connect = require('./connection');
+const connect = require('./connection');
 
-// const dbColletion = 'users';
+const dbColletion = 'users';
 
-// const createUser = async () => {};
+const createUser = async (user) => {
+  const { username, email, password } = user;
+  const db = await connect();
+
+  const newUser = await db.collection(dbColletion).insertOne({ username, email, password });
+
+  return {
+    username: newUser.username,
+    email: newUser.email,
+    id: newUser.insertedId,
+  };
+};
+
 // const findUser = async () => {};
 
-// module.exports = {
-//   createUser,
+module.exports = {
+  createUser,
 //   findUser,
-// };
+};
