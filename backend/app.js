7428f3936313bed3cbc88796291b291697c38a3d
Mariana Garcia <maridgarcia045@gmail.com>
2022-05-18 22:42:53 -0300

    feat: 🎸 create structure to server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,12 @@
-const name = 'john'
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const cors = require('cors');
+
+const { PORT } = process.env;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`App listening on ${PORT}`));
