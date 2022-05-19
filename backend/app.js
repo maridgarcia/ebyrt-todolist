481748b27ec61a5ebf8b05ae36397b610b1d48ee
Mariana Garcia <maridgarcia045@gmail.com>
2022-05-19 13:22:27 -0300

    fix: 🐛 fixed server, dev script and app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
-require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const domainError = require('./utils/errors/domainError');
+const errorHandler = require('./utils/errors/errorHandler');
 
 const app = express();
-const cors = require('cors');
-
-const { PORT } = process.env;
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT, () => console.log(`App listening on ${PORT}`));
+app.use('/register', routes.userRouter);
+
+app.use(domainError);
+app.use(errorHandler);
+
+module.exports = app;
